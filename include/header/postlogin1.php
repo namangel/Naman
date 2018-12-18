6b31933162c617cf79733dd03c2359366c08ffa8
Deepali Panda <deepali4499@gmail.com>
2018-12-18 21:38:44 +0530

    Update postlogin1.php

--- include/header/postlogin1.php
+++ include/header/postlogin1.php
@@ -31,7 +31,7 @@
     <div class="item2">
         <nav id="mainNav" class="stroke">
         <ul>
-            <li><a href="db-home.php">HOME</a></li>
+            <li><a href="../db-home.php">HOME</a></li>
             <li><a href="../StartUp-DB-Overview.php">COMPANY PROFILE</a></li>
             <li><a href="#">TOOLS & SERVICES</a></li>
         </ul>
