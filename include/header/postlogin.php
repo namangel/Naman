98956246e09bc9677b662d70beae0abf9f65c361
Sonal Sharma <sonalshrm22@gmail.com>
2018-12-18 12:46:27 +0530

    postlogin header changes

--- include/header/postlogin.php
+++ include/header/postlogin.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-    <link rel="stylesheet" type="text/css" href="css\db-home.css">
+    <link rel="stylesheet" type="text/css" href="css\postlogin.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Profile</title>
 </head>
